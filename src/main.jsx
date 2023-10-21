84338025b4c9f391c055c77f7daa6e97ac5e7c83
YohanBZ <barbozayohan@gmail.com>
2023-10-21 20:05:56 -0300

    feat: Atualização do arquivo main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Rotas from './Rotas.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Rotas />
   </React.StrictMode>,
-)
+);
