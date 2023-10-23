479eb0427dc2f0a65986340856aeb81bd8c8bc59
Caroli-ne <karoline_gnn@hotmail.com>
2023-10-23 08:44:29 -0300

    feat: adiciona rota para cadastro

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from './style/GlobalStyle';
-import Login from './pages/Login/Login';
+import Login from './pages/Login/Login'; 
+import Cadastro from './pages/Cadastro/Cadastro'
 
 const Rotas = () => {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Cadastro />} />
       </Routes>
     </BrowserRouter>
   );
