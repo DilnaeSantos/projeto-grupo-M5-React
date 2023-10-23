8ad71eb6ba9e7aeb823162436a17066a10fa37b2
Caroli-ne <karoline_gnn@hotmail.com>
2023-10-23 18:52:53 -0300

    feat: adiciona rota para página home

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from './style/GlobalStyle';
 import Login from './pages/Login/Login'; 
 import Cadastro from './pages/Cadastro/Cadastro'
+import Home from './pages/Home/Home';
 
 const Rotas = () => {
   return (
@@ -12,6 +13,7 @@ const Rotas = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Cadastro />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
