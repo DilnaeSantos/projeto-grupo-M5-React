b0f88f93c63441e892e7e5a7ba65eeaa00716ed3
Caroli-ne <karoline_gnn@hotmail.com>
2023-10-23 08:46:31 -0300

    feat: estilo da página de cadastro

---
+++ src/pages/Cadastro/cadastro.styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const StyleContainerCadastro = styled.main`
+
+width: 100%;
+height: 100vh;
+display: flex;
+align-items: center;
+justify-content: center;
+background: #c4ab9a;
+
+.destaque{
+    font-weight:bold;
+}
+
+.content {
+    display: flex;
+    flex-direction: column;
+    padding: 48px;
+    height: fit-content;
+    justify-content: space-between;
+    align-items: center;
+    border-radius: 16px;
+    background: #f5f5f5;
+    box-shadow: -4px -4px 8px 0px rgba(0, 0, 0, 0.25),
+    4px 4px 8px 0px rgba(0, 0, 0, 0.25);
+    gap: 15px;
+}
+
+
+
+
+`;
