c25fc24495b36de7c1041d6525fcd7943989c544
DilnaeSantos <dilnaerennan84@gmail.com>
2023-10-25 17:18:22 -0300

    FEAT: arrumando um conflito

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,12 +6,7 @@ import { StyleContainerLogin } from './login.styles';
 import Button from '../../components/common/Button/Button';
 import diversiartImg from '/diversiart.png';
 import { loginUsuario } from '../../services/api';
-<<<<<<< HEAD
 import { useState } from "react";
-=======
-import { useState } from 'react';
->>>>>>> cd4ef75e35b0edf7ab3a7e2db88ad235eb377047
-
 const Login = () => {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
