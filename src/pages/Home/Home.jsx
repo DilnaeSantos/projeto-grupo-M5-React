0ca6d0fe225058432d6386ddf63094d2adbf193c
L4urenmartins <lauren_martins_borba@hotmail.com>
2023-10-24 21:32:55 -0300

    implementação da pagina Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Header from "../../components/common/Header/Header"
+import ArtesanatoImg from "../../assets/artesanato.png"
+import { HomeStyle } from '../../style/GlobalStyle';
+import ArtesaImg from "../../assets/artesa.png"
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <div className='home'>
+      <HomeStyle />
+
+      <main>
+          <h2>Bem vindo a Diversiart
+              <p>E nosso objetivo com o diversiArt é promover autenticidade, qualidade e inclusão dos produtos oferecidos por artesãos e empreendedores de grupos minorizados no marketplace DiversiArt.</p>
+              <img className='artesanato' src={ArtesanatoImg} />
+          </h2>
+          <h2>Faça seu cadastro agora mesmo!
+              <p>Todos os vendedores que desejam se registrar no DiversiArt podem acessar a nossa aba "Cadastro" e cadastrar todas as artes.</p>
+              <img className='artesa' src={ArtesaImg} />
+          </h2>
+      </main>
+    </div>
+  );
 }
 
 export default Home
