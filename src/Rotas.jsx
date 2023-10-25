0345e1dd69f757d570ec86ba5e7e2cfb35fbe33b
YohanBZ <barbozayohan@gmail.com>
2023-10-25 16:01:40 -0300

    feat: :zap: atualização das rotas no browser routes

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -2,9 +2,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from './style/GlobalStyle';
-import Login from './pages/Login/Login'; 
-import Cadastro from './pages/Cadastro/Cadastro'
+import Login from './pages/Login/Login';
+import Cadastro from './pages/Cadastro/Cadastro';
 import Home from './pages/Home/Home';
+import Artesao from './pages/Artesao/Artesao';
 
 const Rotas = () => {
   return (
@@ -13,6 +14,7 @@ const Rotas = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/artesao" element={<Artesao />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
